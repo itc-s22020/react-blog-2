2dca8820325fb617fd8c1762c7d6b4ec1409e1ba
itc-s22020 <s22020@std.it-college.ac.jp>
2023-02-14 07:15:57 +0000

    chapter 1.7

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,16 @@ import Image from "next/image";
 
 const Home = () => {
   return (
-    <h1>CUBE</h1>
+    <>
+      <header>HEADER</header>
+      <main>
+        <div>
+          <h1>CUBE</h1>
+          <p>アウトプットしていくサイト</p>
+        </div>
+      </main>
+      <footer>FOOTER</footer>
+    </>
   )
 }
 
