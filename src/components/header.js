d762da691bc487b9c58d9c18294d50f47877dc8e
itc-s22020 <s22020@std.it-college.ac.jp>
2023-02-15 06:50:26 +0000

    chapter 4.2

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,14 @@
 import Logo from "./logo"
 import Nav from "./nav"
+import styles from "@/styles/header.module.css"
 
 const Header = () => {
     return (
         <header>
-            <Logo boxOn />
-            <Nav />
+            <div className={styles.flexContainer}>
+                <Logo boxOn />
+                <Nav />
+            </div>
         </header>
     )
 }
